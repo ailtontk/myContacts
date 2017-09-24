55f76eb4b84af66cd466d02f9db519df2f277f5c
Ailton Ramos <ailton.tk@hotmail.com>
2017-09-24 17:05:09 -0300

    Contact Info screen logic created

--- app/src/main/java/net/artgamestudio/rgatest/ui/activities/ContactInfoActivity.java
+++ app/src/main/java/net/artgamestudio/rgatest/ui/activities/ContactInfoActivity.java
@@ -1,10 +1,12 @@
 package net.artgamestudio.rgatest.ui.activities;
 
+import android.content.Intent;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -27,9 +29,15 @@ public class ContactInfoActivity extends BaseActivity {
     @BindView(R.id.colToolbar) CollapsingToolbarLayout colToolbar;
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.ivPhoto) ImageView ivPhoto;
+    @BindView(R.id.tvEmail) TextView tvEmail;
+    @BindView(R.id.tvBorn) TextView tvBorn;
+    @BindView(R.id.tvBio) TextView tvBio;
 
     /***** VARIABLES *****/
     private Contact mContact;
+
+    /***** CONSTANTS *****/
+    private final int REQUEST_EDIT_CONTACT = 1;
 
     @Override
     public int setView() throws Exception {
@@ -55,7 +63,17 @@ public class ContactInfoActivity extends BaseActivity {
                 .apply(requestOptions)
                 .into(ivPhoto);
 
-        colToolbar.setTitle(mContact.getName());
+        addContactInfoOnFields(mContact);
+    }
+
+    /**
+     * Adds the contact info on screen
+     */
+    private void addContactInfoOnFields(Contact contact) throws Exception {
+        colToolbar.setTitle(contact.getName());
+        tvEmail.setText(contact.getEmail());
+        tvBorn.setText(contact.getBorn());
+        tvBio.setText(contact.getBio());
     }
 
     @Override
@@ -74,5 +92,20 @@ public class ContactInfoActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            //If its coming from edit contact screen, updates the contact object and updates screen
+            if (requestCode == REQUEST_EDIT_CONTACT) {
+                mContact = data.getParcelableExtra(Param.Intent.CONTACT);
+                addContactInfoOnFields(mContact);
+            }
+        } catch (Exception error) {
+            Log.e("Error", "Error at onActivityResult in " + getClass().getName() + ". " + error.getMessage());
+        }
     }
 }
